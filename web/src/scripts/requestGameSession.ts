7e02ea08cfbac75402d82077ffa98e6dbae2ef35
Dominic Brauer <dominicbrauer06@gmail.com>
2025-04-19 23:56:12 +0200

    Refactoring WordGrid logic

---
+++ web/src/scripts/requestGameSession.ts
@@ -0,0 +1,16 @@
+import type { GameSession } from "../lib/gameSession";
+
+export const gameSession: GameSession = await (async function requestGameSession(): Promise<GameSession> {
+  const response = await fetch('http://localhost:8080/api/start-game', {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.statusText}`);
+  }
+  return await response.json() as GameSession;
+})();
