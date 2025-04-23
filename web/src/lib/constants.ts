4d4eff2560be71d69a8354cdb99ac05e3272551c
Dominic Brauer <dominicbrauer06@gmail.com>
2025-04-23 10:25:52 +0200

    Refactor game site by putting functions where they belong

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,7 +1,5 @@
-const KEYBOARD_KEYS: Array<Array<string>> = [
+export const KEYBOARD_KEYS: Array<Array<string>> = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
 ];
-
-export {KEYBOARD_KEYS};
